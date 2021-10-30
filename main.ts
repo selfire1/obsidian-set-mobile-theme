4e5565be5117c505d279e8200bee9466f2597217
selfire1 <try2banonym@gmail.com>
2021-10-30 15:13:46 +0200

    init
    generally works, adding a new theme still breaks it

--- main.ts
+++ main.ts
@@ -9,86 +9,72 @@ import {
   Setting,
 } from "obsidian";
 
-interface MyPluginSettings {
+interface SetMobileThemeSettings {
+  mobileTheme: any;
+  desktopTheme: any;
+  themesObject: any;
   mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-  mySetting: "default",
+const DEFAULT_SETTINGS: SetMobileThemeSettings = {
+  mobileTheme: "Obsidian You",
+  desktopTheme: "Minimal",
+  themesObject: "none",
+  mySetting: "default"
 };
 
 export default class MyPlugin extends Plugin {
-  settings: MyPluginSettings;
+  settings: SetMobileThemeSettings;
 
   async onload() {
     await this.loadSettings();
-
-    // This creates an icon in the left ribbon.
-    let ribbonIconEl = this.addRibbonIcon(
-      "dice",
-      "Sample Plugin",
-      (evt: MouseEvent) => {
-        // Called when the user clicks the icon.
-        new Notice("This is a notice!");
-      }
-    );
-    // Perform additional things with the ribbon
-    ribbonIconEl.addClass("my-plugin-ribbon-class");
-
-    // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-    let statusBarItemEl = this.addStatusBarItem();
-    statusBarItemEl.setText("Status Bar Text");
-
-    // This adds a simple command that can be triggered anywhere
-    this.addCommand({
-      id: "open-sample-modal-simple",
-      name: "Open sample modal (simple)",
-      callback: () => {
-        new SampleModal(this.app).open();
-      },
-    });
-    // This adds an editor command that can perform some operation on the current editor instance
-    this.addCommand({
-      id: "sample-editor-command",
-      name: "Sample editor command",
-      editorCallback: (editor: Editor, view: MarkdownView) => {
-        console.log(editor.getSelection());
-        editor.replaceSelection("Sample Editor Command");
-      },
-    });
-    // This adds a complex command that can check whether the current state of the app allows execution of the command
-    this.addCommand({
-      id: "open-sample-modal-complex",
-      name: "Open sample modal (complex)",
-      checkCallback: (checking: boolean) => {
-        // Conditions to check
-        let markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-        if (markdownView) {
-          // If checking is true, we're simply "checking" if the command can be run.
-          // If checking is false, then we want to actually perform the operation.
-          if (!checking) {
-            new SampleModal(this.app).open();
-          }
-
-          // This command will only show up in Command Palette when the check function returns true
-          return true;
-        }
-      },
-    });
-
+    this.loadThemeObject();
+    this.setThemeByDevice();
     // This adds a settings tab so the user can configure various aspects of the plugin
     this.addSettingTab(new SampleSettingTab(this.app, this));
 
-    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-    // Using this function will automatically remove the event listener when this plugin is disabled.
-    this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-      console.log("click", evt);
-    });
-
-    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-    this.registerInterval(
-      window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+    this.registerEvent(
+      this.app.workspace.on("css-change", () => {
+        console.log("noticed change!")
+        // @ts-ignore
+        if (!this.app.isMobile) {
+          // @ts-ignore
+          this.settings.desktopTheme = this.app.customCss.theme;
+          this.saveSettings();
+        } else {
+          // @ts-ignore
+          this.settings.mobileTheme = this.app.customCss.theme;
+          this.saveSettings();
+        }
+        })
     );
+
+  }
+
+  loadThemeObject() {
+    // @ts-ignore
+    const themeArr = this.app.customCss.themes;
+    const themeObj = Object.assign({}, themeArr);
+    this.settings.themesObject = themeObj;
+    this.saveSettings();
+    console.log("Set Mobile Theme: Saved loaded themes");
+  }
+
+  setThemeByDevice() {
+    // @ts-ignore
+    if (!this.app.isMobile) {
+      console.log("Set to Desktop theme");
+      // @ts-ignore
+      this.app.customCss.setTheme(this.settings.desktopTheme);
+    } else {
+      console.log("Set to Mobile theme");
+      // @ts-ignore
+      this.app.customCss.setTheme(this.settings.mobileTheme);
+    }
+  }
+
+  getKeyByValue(object: { [x: string]: any }, value: any) {
+    return Object.keys(object).find((key) => object[key] === value);
   }
 
   onunload() {}
@@ -99,22 +85,6 @@ export default class MyPlugin extends Plugin {
 
   async saveSettings() {
     await this.saveData(this.settings);
-  }
-}
-
-class SampleModal extends Modal {
-  constructor(app: App) {
-    super(app);
-  }
-
-  onOpen() {
-    let { contentEl } = this;
-    contentEl.setText("Woah!");
-  }
-
-  onClose() {
-    let { contentEl } = this;
-    contentEl.empty();
   }
 }
 
@@ -129,22 +99,60 @@ class SampleSettingTab extends PluginSettingTab {
   display(): void {
     let { containerEl } = this;
 
-    containerEl.empty();
-
-    containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
-
+    containerEl.createEl("h2", { text: "Set Mobile Theme: Settings" });
+    
+    containerEl.createEl("h3", { text: "Desktop 🖥" });
     new Setting(containerEl)
-      .setName("Setting #1")
-      .setDesc("It's a secret")
-      .addText((text) =>
-        text
-          .setPlaceholder("Enter your secret")
-          .setValue(this.plugin.settings.mySetting)
+      .setName("Desktop Theme")
+      .setDesc("Choose a theme for deskop")
+      .addDropdown((dropdown) =>
+        dropdown
+          .addOptions(this.plugin.settings.themesObject)
+          .setValue(
+            // @ts-ignore
+            this.plugin.getKeyByValue(
+              this.plugin.settings.themesObject,
+              this.plugin.settings.desktopTheme
+            )
+          )
           .onChange(async (value) => {
-            console.log("Secret: " + value);
-            this.plugin.settings.mySetting = value;
+            const themeObj = this.plugin.settings.themesObject;
+            this.plugin.settings.desktopTheme = themeObj[value];
             await this.plugin.saveSettings();
+            this.plugin.setThemeByDevice();
           })
       );
+    
+    containerEl.createEl("h3", { text: "Mobile 📱" });
+    new Setting(containerEl)
+      .setName("Mobile Theme")
+      .setDesc("Choose a theme for mobile")
+      .addDropdown((dropdown) =>
+        dropdown
+          .addOptions(this.plugin.settings.themesObject)
+          .setValue(
+            // @ts-ignore
+            this.plugin.getKeyByValue(
+              this.plugin.settings.themesObject,
+              this.plugin.settings.mobileTheme
+            )
+          )
+          .onChange(async (value) => {
+            const themeObj = this.plugin.settings.themesObject;
+            this.plugin.settings.mobileTheme = themeObj[value];
+            await this.plugin.saveSettings();
+            this.plugin.setThemeByDevice();
+          })
+      );
+    
+
+    new Setting(containerEl)
+    .addButton((cb) =>
+      cb
+        .setButtonText("Reload themes")
+        .onClick(() => {
+          this.plugin.loadThemeObject();
+        })
+    );
   }
 }
